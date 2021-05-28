050d1999e402a6fdccf6b66855f1157faf06979d
Henrique Silva <josehs555@gmail.com>
2021-05-28 10:43:38 -0300

    Initial Commit

---
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.aboutme
+
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val myName: MyName = MyName(name = "Henrique Silva")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.myName = myName
+
+        binding.doneButton.setOnClickListener {
+            addNickName(it)
+        }
+
+        //findViewById<Button>(R.id.done_button).setOnClickListener {
+            //addNickName(it)
+        //}
+    }
+
+    private fun addNickName(view:View) {
+
+        binding.apply {
+            //nicknameText.text = binding.nicknameEdit.text
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+}
